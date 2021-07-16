e1c0fb63f713bd40e8fd5c42468bc2ffc04a0d73
maedwards3 <michael.edwards.4108@gmail.com>
2021-07-15 21:37:27 -0400

    Created ratings controller.

---
+++ eCommerceStarterCode/Controllers/RatingsController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using eCommerceStarterCode.Data;
+using eCommerceStarterCode.Models;
+
+
+namespace eCommerceStarterCode.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RatingsController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+       
+        public RatingsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // <baseurl/api/ratings/>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var ratings = _context.Ratings;
+            return Ok(ratings);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Rating value)
+        {
+            _context.Ratings.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
+    }
+}
