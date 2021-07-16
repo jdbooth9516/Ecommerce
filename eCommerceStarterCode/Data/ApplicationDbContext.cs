1ca658c06c2b8e77bf3c14242df5f01bc14bfef6
jeremiah <jboothwebdev@gmail.com>
2021-07-15 17:02:21 -0700

    reinit

---
+++ eCommerceStarterCode/Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using eCommerceStarterCode.Configuration;
+using eCommerceStarterCode.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceStarterCode.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            :base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
+        public DbSet<Rating> Ratings { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfiguration(new RolesConfiguration());
+
+           // modelBuilder.Entity<Product>()
+           //     .HasData(
+           //         new Product { ProductId = 1, Name = "Playstation 5", Discription = "Advanced Gaming console", CategoryId = 1, }
+           //     );
+        }
+
+    }
+}
