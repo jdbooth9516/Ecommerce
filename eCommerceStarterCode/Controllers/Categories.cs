3ce1f45f952e23f8e364ee22bb7c7df9b6c9010f
jeremiah <jboothwebdev@gmail.com>
2021-07-15 23:22:47 -0700

    update categories

--- eCommerceStarterCode/Controllers/Categories.cs
+++ eCommerceStarterCode/Controllers/Categories.cs
@@ -2,10 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using eCommerceStarterCode.Data;
+using Microsoft.AspNetCore.Mvc;
+using eCommerceStarterCode.Models;
 
 namespace eCommerceStarterCode.Controllers
 {
-    public class Categories
+    [Route("api/categories")]
+    [ApiController]
+
+    public class CategoriesController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //<baseurl>/api/examples/user
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var category = _context.Categories;
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Category value)
+        {
+            _context.Categories.Add(value);
+            _context.SaveChanges();
+            return StatusCode(201, value);
+        }
     }
 }
